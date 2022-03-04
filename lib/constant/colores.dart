d93ec3dcdc9899813c655e351cc8b9b3f24610aa
Abel Fernando Sangama Oñate <abelsangama@upeu.edu.pe>
2022-03-03 23:39:27 -0500

    color

--- lib/constant/colores.dart
+++ lib/constant/colores.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 Color primaryTextColor = Color(0xFFB11F24);
 Color secondaryTextColor = Color(0xFFF49C00);
 Color titleTextoSCUROColor = Color(0xFFFFFFFF);
+//Color titleTexto02SCUROColor = Color.fromARGB(255, 145, 22, 22);
